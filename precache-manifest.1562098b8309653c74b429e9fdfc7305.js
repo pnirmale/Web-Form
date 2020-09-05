ead41875e5e5df2774fc46f6433a23be2ab3c45b
pnirmale52 <prathamapu5259@gmail.com>
2020-09-05 13:14:56 +0530

    Updates

---
+++ precache-manifest.1562098b8309653c74b429e9fdfc7305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b86d137f87c5b4347984f09bc1fad4a",
+    "url": "/Web-Form/index.html"
+  },
+  {
+    "revision": "bf5b46cb830754ade22a",
+    "url": "/Web-Form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b570d623c694f2ef1916",
+    "url": "/Web-Form/static/js/2.1d00f90e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web-Form/static/js/2.1d00f90e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5b46cb830754ade22a",
+    "url": "/Web-Form/static/js/main.7c8f7488.chunk.js"
+  },
+  {
+    "revision": "1d78e39278b247636886",
+    "url": "/Web-Form/static/js/runtime-main.8aa512aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web-Form/static/media/logo.5d5d9eef.svg"
+  }
+]);
